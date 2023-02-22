3f75ca595db8346862843572395818add99c1b9f
Arnab Sen <arnabsen1729@gmail.com>
2023-02-22 16:12:36 +0530

    new tests added

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -1,21 +1,60 @@
 const todoList = require("../todo");
 
-const { all, add, markAsComplete } = todoList();
+const { all, add, markAsComplete, overdue, dueToday, dueLater } = todoList();
 
 describe("TodoList Test Suite", () => {
-  test("add a todo item", () => {
-    expect(all.length).toBe(0);
+  beforeAll(() => {
+    const today = new Date();
+    const yesterday = new Date(today);
+    yesterday.setDate(yesterday.getDate() - 1);
+
+    const tomorrow = new Date(today);
+    tomorrow.setDate(tomorrow.getDate() + 1);
+    add({
+      title: "Today's task",
+      dueDate: today.toISOString().split("T")[0],
+      completed: false,
+    });
+    add({
+      title: "Yesterday's task",
+      dueDate: yesterday.toISOString().split("T")[0],
+      completed: false,
+    });
+    add({
+      title: "Tomorrow's task",
+      dueDate: tomorrow.toISOString().split("T")[0],
+      completed: false,
+    });
+  });
+
+  test("creating a new todo", () => {
+    const length = all.length;
     add({
       title: "Buy milk",
       dueDate: new Date().toISOString().split("T")[0],
       completed: false,
     });
-    expect(all.length).toBe(1);
+    expect(all.length).toBe(length + 1);
   });
 
-  test("mark as complete", () => {
+  test("marking a todo as completed", () => {
     expect(all[0].completed).toBe(false);
     markAsComplete(0);
     expect(all[0].completed).toBe(true);
+  });
+
+  test("retrieval of overdue items", () => {
+    const overdueItems = overdue();
+    expect(overdueItems.length).toBe(1);
+  });
+
+  test("retrieval of due today items", () => {
+    const dueTodayItems = dueToday();
+    expect(dueTodayItems.length).toBe(2);
+  });
+
+  test(" retrieval of due later items", () => {
+    const dueLaterItems = dueLater();
+    expect(dueLaterItems.length).toBe(1);
   });
 });
