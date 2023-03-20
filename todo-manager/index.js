e9fa69e94f53f5463df55bcda24ff98e6237cce7
Arnab Sen <arnabsen1729@gmail.com>
2023-03-20 10:03:15 +0530

    Initialized ESLint

--- todo-manager/index.js
+++ todo-manager/index.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 app.listen(3000, () => {
-    console.log('Server running on port 3000');
+  console.log("Server running on port 3000");
 });
